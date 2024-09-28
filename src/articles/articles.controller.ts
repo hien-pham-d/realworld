de16f7a25ecdd60a15faa91f5d8d7349f4cf7675
hien-pham-d <dh.pham.dev@gmail.com>
2024-09-28 13:01:42 +0900

    refactor: change return format of service method

--- src/articles/articles.controller.ts
+++ src/articles/articles.controller.ts
@@ -1,51 +1,19 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { GetArticlesResp, FindByQueryDto } from './dto/get-articles.dto';
 import { ArticlesService } from './articles.service';
-import { Article, Author } from './entities/findBy.entity';
 
 @Controller()
 export class ArticlesController {
   constructor(private articleService: ArticlesService) {}
-  curUser = new Author({
-    id: 1,
-    username: 'default',
-    bio: 'bio',
-    image: '',
-    followedBy: [],
-  });
+
   @Get()
   async findBy(@Query() query: FindByQueryDto): Promise<GetArticlesResp> {
-    return this.mapArticlesToRespDto(
-      await this.articleService.findBy({
-        tag: query.tag,
-        author: query.author,
-        favorited: query.favorited,
-        limit: query.limit,
-        offset: query.offset,
-      }),
-    );
-  }
-  mapArticlesToRespDto(articles: Article[]): GetArticlesResp {
-    return {
-      articles: articles.map((article) => {
-        return {
-          slug: article.slug,
-          title: article.title,
-          description: article.description,
-          tagList: article.tags,
-          createdAt: article.createdAt.toISOString(),
-          updatedAt: article.updatedAt.toISOString(),
-          favorited: article.isFavoritedBy(this.curUser.id),
-          favoritesCount: article.favoritedBy.length,
-          author: {
-            username: article.author.username,
-            bio: article.author.bio,
-            image: article.author.image,
-            following: article.author.isFollowedBy(this.curUser.id),
-          },
-        };
-      }),
-      articlesCount: articles.length,
-    };
+    return await this.articleService.findBy({
+      tag: query.tag,
+      author: query.author,
+      favorited: query.favorited,
+      limit: query.limit,
+      offset: query.offset,
+    });
   }
 }
--- test/articles/articles.e2e-spec.ts
+++ test/articles/articles.e2e-spec.ts
@@ -1,21 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { PrismaClient } from '@prisma/client';
+import { ArticlesModule } from '../../src/articles/articles.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+      imports: [ArticlesModule],
+    })
+      .overrideProvider(PrismaClient)
+      .useClass(PrismaClient)
+      .compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('GET /api/articles', () => {
     return request(app.getHttpServer()).get('/api/articles').expect(200);
   });
 });
