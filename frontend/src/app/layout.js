9bc3040cf398d8bd65a80174c23acb9fdccfa303
hien-pham-d <dh.pham.dev@gmail.com>
2025-01-16 02:51:52 +0900

    chore(fe): minimalize fe code

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,15 +1,3 @@
-import { Geist, Geist_Mono } from "next/font/google";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata = {
   title: "Real World",
   description: "My Real World web app built with Next.js",
@@ -17,10 +5,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
+    <html>
+      <body>{children}</body>
     </html>
   );
 }
