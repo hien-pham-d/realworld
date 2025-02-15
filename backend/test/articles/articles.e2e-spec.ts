4ddc5b495c2386a8d182e2de8ee96f2bda51b7fb
hien-pham-d <dh.pham.dev@gmail.com>
2025-02-16 04:54:47 +0900

    fix: data validation

--- backend/test/articles/articles.e2e-spec.ts
+++ backend/test/articles/articles.e2e-spec.ts
@@ -3,67 +3,88 @@ import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { ArticlesModule } from '../../src/articles/articles.module';
 import { PrismaClient } from '@prisma/client';
-import { FindByQueryDto } from 'src/articles/dto/get-articles.dto';
 
-describe('Integration Test', () => {
-  let app: INestApplication;
-  let prisma: PrismaClient = new PrismaClient();
+let app: INestApplication;
+let prisma: PrismaClient = new PrismaClient();
 
-  beforeAll(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [ArticlesModule],
-    }).compile();
+beforeAll(async () => {
+  const moduleFixture: TestingModule = await Test.createTestingModule({
+    imports: [ArticlesModule],
+  }).compile();
 
-    app = moduleFixture.createNestApplication();
-    app.useGlobalPipes(
-      new ValidationPipe({
-        transform: true,
-      }),
-    );
-    await app.init();
+  app = moduleFixture.createNestApplication();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+  await app.init();
 
-    await truncateDatabase(prisma);
+  await truncateDatabase(prisma);
+});
+
+afterAll(async () => {
+  await prisma.$disconnect();
+  await app.close();
+});
+
+beforeEach(async () => {
+  await prisma.author.create({
+    data: {
+      id: 1,
+      username: 'default',
+      bio: 'I am the default user of this site',
+      image: 'this should be an image',
+    },
   });
+});
 
-  afterAll(async () => {
-    await prisma.$disconnect();
-    await app.close();
-  });
+afterEach(async () => {
+  await truncateDatabase(prisma);
+});
 
-  beforeEach(async () => {
-    await prisma.author.create({
-      data: {
-        id: 1,
-        username: 'default',
-        bio: 'I am the default user of this site',
-        image: 'this should be an image',
+it('Should write/read an article successfully via POST/GET /articles', async () => {
+  const response = await request(app.getHttpServer())
+    .post('/articles')
+    .send({
+      article: {
+        body: 'body-test',
+        description: 'description-test',
+        title: 'title-test',
+        tagList: ['tag1-test', 'tag2-test'],
       },
     });
+
+  expect(response.status).toBe(201);
+  expect(response.body).toEqual({
+    article: {
+      slug: expect.any(String),
+      title: 'title-test',
+      description: 'description-test',
+      body: 'body-test',
+      tagList: expect.arrayContaining(['tag1-test', 'tag2-test']),
+      createdAt: expect.any(String),
+      updatedAt: expect.any(String),
+      favorited: false,
+      favoritesCount: 0,
+      author: {
+        bio: 'I am the default user of this site',
+        following: false,
+        image: 'this should be an image',
+        username: 'default',
+      },
+    },
   });
 
-  afterEach(async () => {
-    await truncateDatabase(prisma);
-  });
+  const getResponse = await request(app.getHttpServer()).get('/articles');
 
-  it('Should write/read an article successfully via POST/GET /articles', async () => {
-    const response = await request(app.getHttpServer())
-      .post('/articles')
-      .send({
-        article: {
-          body: 'body-test',
-          description: 'description-test',
-          title: 'title-test',
-          tagList: ['tag1-test', 'tag2-test'],
-        },
-      });
-
-    expect(response.status).toBe(201);
-    expect(response.body).toEqual({
-      article: {
+  expect(getResponse.status).toBe(200);
+  expect(getResponse.body).toEqual({
+    articles: expect.arrayContaining([
+      {
         slug: expect.any(String),
         title: 'title-test',
         description: 'description-test',
-        body: 'body-test',
         tagList: expect.arrayContaining(['tag1-test', 'tag2-test']),
         createdAt: expect.any(String),
         updatedAt: expect.any(String),
@@ -76,61 +97,76 @@ describe('Integration Test', () => {
           username: 'default',
         },
       },
-    });
-
-    const getResponse = await request(app.getHttpServer()).get('/articles');
-
-    expect(getResponse.status).toBe(200);
-    expect(getResponse.body).toEqual({
-      articles: expect.arrayContaining([
-        {
-          slug: expect.any(String),
-          title: 'title-test',
-          description: 'description-test',
-          tagList: expect.arrayContaining(['tag1-test', 'tag2-test']),
-          createdAt: expect.any(String),
-          updatedAt: expect.any(String),
-          favorited: false,
-          favoritesCount: 0,
-          author: {
-            bio: 'I am the default user of this site',
-            following: false,
-            image: 'this should be an image',
-            username: 'default',
-          },
-        },
-      ]),
-      articlesCount: 1,
-    });
+    ]),
+    articlesCount: 1,
   });
+});
 
-  describe('Should return validation error (400) when GET /articles with an invalid query parameter.', () => {
-    const cases: {
-      query: Record<string, any>;
-      errMessage: string;
-    }[] = [
-      {
-        query: { limit: 'invalid' },
-        errMessage: 'limit must be an integer number',
-      },
-      {
-        query: { limit: '0' },
-        errMessage: 'limit must not be less than 1',
-      },
-      {
-        query: { tag: '' },
-        errMessage: 'tag should not be empty',
-      },
-    ];
+describe('GET /articles validation error', () => {
+  const cases: {
+    query: Record<string, any>;
+    errMessage: string;
+  }[] = [
+    {
+      query: { limit: 'invalid' },
+      errMessage: 'limit must be an integer number',
+    },
+    {
+      query: { limit: '0' },
+      errMessage: 'limit must not be less than 1',
+    },
+    {
+      query: { tag: '' },
+      errMessage: 'tag should not be empty',
+    },
+  ];
 
-    test.each(cases)('$errMessage', async ({ query, errMessage }) => {
-      const response = await request(app.getHttpServer()).get(
-        `/articles?${new URLSearchParams(query).toString()}`,
-      );
+  test.each(cases)('$errMessage', async ({ query, errMessage }) => {
+    const response = await request(app.getHttpServer()).get(
+      `/articles?${new URLSearchParams(query).toString()}`,
+    );
 
-      expect(response.status).toBe(400);
-      expect(response.body.message).toContain(errMessage);
-    });
+    expect(response.status).toBe(400);
+    expect(response.body.message).toContain(errMessage);
+  });
+});
+
+describe('POST /articles validation error', () => {
+  const cases: {
+    body: Record<string, any>;
+    errMessage: string;
+  }[] = [
+    {
+      body: {
+        article: {
+          body: 'body-test',
+          description: 'description-test',
+          title: '',
+          tagList: ['tag1-test', 'tag2-test'],
+        },
+      },
+      errMessage: 'article.title should not be empty',
+    },
+    {
+      body: {
+        article: {
+          body: 'body-test',
+          description: 'description-test',
+          title: 'title-test',
+          tagList: ['tag1-test', ''],
+        },
+      },
+      errMessage: 'article.each value in tagList should not be empty',
+    },
+  ];
+
+  test.each(cases)('$errMessage', async ({ body, errMessage }) => {
+    const response = await request(app.getHttpServer())
+      .post('/articles')
+      .send(body);
+
+    expect(response.status).toBe(400);
+    expect(response.body.message).toContain(errMessage);
   });
 });
 
